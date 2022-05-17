8dece94e60495fe4e6cbab8de6458ec22e8c868b
Amir H. Jabari <amirhamzehjabari@gmail.com>
2022-05-17 19:16:41 +0430

    update controllers

--- src/WebApi/Controllers/ApiControllerBase.cs
+++ src/WebApi/Controllers/ApiControllerBase.cs
@@ -7,9 +7,12 @@ namespace Weblog.WebApi.Controllers;
 [ApiController]
 [ApiVersion("1")]
 [Route("api/v{version:apiVersion}/[controller]")]
-public abstract class ApiControllerBase : ControllerBase
+public abstract class ApiControllerBase<TController> : ControllerBase
+    where TController : ApiControllerBase<TController>
 {
     private ISender _mediator = null!;
+    private ILogger<TController> _logger = null!;
 
     protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    protected ILogger<TController> Logger => _logger ??= HttpContext.RequestServices.GetRequiredService<ILogger<TController>>();
 }
--- src/WebApi/Controllers/V1/BlogsController.cs
+++ src/WebApi/Controllers/V1/BlogsController.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Mvc.Formatters;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Weblog.WebApi.Controllers.V1;
 
 [ApiVersion("1")]
-public class BlogsController : ApiControllerBase
+public class BlogsController : ApiControllerBase<BlogsController>
 {
     [HttpGet("[action]")]
     public ActionResult Get(string name = "Amir")
