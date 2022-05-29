20742b280cd318c48b04dd54d91f252656ffe818
Amir H. Jabari <amirhamzehjabari@gmail.com>
2022-05-29 23:59:23 +0430

    add theme stuf Mud.

---
+++ src/WebWasm/Shared/MainLayout.razor.cs
@@ -0,0 +1,121 @@
+namespace Weblog.WebWasm.Shared;
+
+public partial class MainLayout : LayoutComponentBase
+{
+    [Inject]
+    public ILocalStorageService LocalStorage { get; set; }
+    private string _themeIcon;
+    bool _drawerOpen = true;
+    private string _themeName = "light";
+    MudTheme _currentTheme = new()
+    {
+        Palette = new Palette
+        {
+            Primary = Colors.Green.Default,
+            Secondary = Colors.Amber.Default
+        }
+    };
+    MudTheme _darkTheme = new()
+    {
+        Palette = new() 
+        {
+            AppbarBackground = "#0097ff",
+            AppbarText = "#ffffff",
+            Primary = "#007cd1",
+            TextPrimary = "#ffffff",
+            Background = "#001524",
+            TextSecondary = "#e2eef6",
+            DrawerBackground = "#093958",
+            DrawerText  = "#ffffff",
+            Surface = "#093958",
+            ActionDefault = "#0097ff",
+            ActionDisabled = "#2f678c",
+            TextDisabled = "#b0b0b0",
+        },
+    };
+    MudTheme _lightTheme = new()
+    {
+        Palette = new() 
+        {
+            AppbarBackground = "#0097ff",
+            AppbarText = "#ffffff",
+            Primary = "#007cd1",
+            TextPrimary = "#0c1217",
+            Background = "#f4fdff",
+            TextSecondary = "#0c1217",
+            DrawerBackground = "#c5e5ff",
+            DrawerText  = "#0c1217",
+            Surface = "#c5e5ff",
+            ActionDefault = "#0c1217",
+            ActionDisabled = "#2f678c",
+            TextDisabled = "#676767",
+            
+        },
+    };
+
+    void DrawerToggle()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        _themeName = await LocalStorage.GetItemAsStringAsync("theme");
+        // UpdateTheme();
+        StateHasChanged();
+    }
+
+    private async Task ChangeThemeAsync()
+    {
+        if (_themeName is "light")
+        {
+            _themeName = "dark";
+        }
+        else if (_themeName is "dark")
+        {
+            _themeName = "light";
+        }
+
+        await LocalStorage.SetItemAsStringAsync("theme", _themeName);
+        // UpdateTheme();
+    }
+
+    // private void UpdateTheme()
+    // {
+    //     if (_themeName is "light")
+    //     {
+    //         _themeIcon = Icons.Filled.LightMode;
+    //         _currentTheme = _darkTheme;
+    //     }
+    //     else if (_themeName is "dark")
+    //     {
+    //         _themeIcon = Icons.Outlined.DarkMode;
+    //         _currentTheme = _lightTheme;
+    //     }
+    // }
+
+
+    private ThemeManagerTheme _themeManager = new ThemeManagerTheme();
+    bool _themeManagerOpen = false;
+
+    void OpenThemeManager(bool value)
+    {
+        _themeManagerOpen = value;
+    }
+
+    void UpdateTheme(ThemeManagerTheme value)
+    {
+        if (_themeName is "light")
+        {
+            _themeIcon = Icons.Filled.LightMode;
+            _currentTheme = _darkTheme;
+        }
+        else if (_themeName is "dark")
+        {
+            _themeIcon = Icons.Outlined.DarkMode;
+            _currentTheme = _lightTheme;
+        }
+        _themeManager = value;
+        StateHasChanged();
+    }
+}
---
+++ src/WebWasm/_Imports.cs
@@ -0,0 +1,8 @@
+global using Blazored.LocalStorage;
+global using Weblog.WebWasm;
+global using Weblog.WebWasm.Interfaces;
+global using Weblog.WebWasm.Services;
+global using MudBlazor.Services;
+global using Microsoft.AspNetCore.Components;
+global using MudBlazor;
+global using MudBlazor.ThemeManager;
